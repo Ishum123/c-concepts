1cb77340cc38174b4cf930ca82bf2233e1dc1677
Yashasweni <94554260+Ishum123@users.noreply.github.com>
2022-07-12 18:43:57 +0530

    Create bst.cpp

---
+++ bst.cpp
@@ -0,0 +1,8 @@
+# Facts: Successor and inorder successor are different things!
+
+Q1) Deleting node in a BST
+# Three posibilities: whether a leaf node, a node with left/right child, root node(in this case inorder successor is needed)!
+
+Code:
+Link: Leetcode-> https://leetcode.com/submissions/detail/745176292/
+
