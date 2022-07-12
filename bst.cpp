815e588d5f74502667c8f6a2f96e3470c043aef4
Yashasweni <94554260+Ishum123@users.noreply.github.com>
2022-07-12 18:57:58 +0530

    Update bst.cpp

--- bst.cpp
+++ bst.cpp
@@ -4,5 +4,6 @@ Q1) Deleting node in a BST
 # Three posibilities: whether a leaf node, a node with left/right child, root node(in this case inorder successor is needed)!
 
 Code:
-Link: Leetcode-> https://leetcode.com/submissions/detail/745176292/
+Link: Leetcode-> https://leetcode.com/submissions/detail/745188769/
+
 
