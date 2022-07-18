a2a32428605dd819d6b5bf2b7bce390e4a0e7e1a
Yashasweni Mathur <94554260+Ishum123@users.noreply.github.com>
2022-07-18 12:26:49 +0530

    Update Graphs.cpp

--- Graphs.cpp
+++ Graphs.cpp
@@ -5,13 +5,15 @@ Link: https://www.codechef.com/problems/REVERSE
 1) Think Greedily!
 2) Make anothr edge into same graph of weight 1, that means reverse edge we created and weight of original edge=0
 3) Apply Dijstra to find shortest path among them.
-4) If distance is still inf, we can't reach that node, else return distance.
+4) If distance is still inf, we can't' reach that node, else return distance.
   
 Trying....
 //Code
 https://www.codechef.com/viewsolution/68787528
 (use 0-1 BFS for better time complexity than Dijstra).
 (Everything about BFs in notes).
+  
+ ----------------------------------------------------------------------------------------------------------------------------------------------------------------------  
   
 Q2) Alien Dictionary
 Link: https://practice.geeksforgeeks.org/problems/alien-dictionary/1
@@ -67,3 +69,181 @@ void dfs(vector<int> adj[], vector<int> &vis, stack<int> &st,int node){
       }
       return ans;
     }
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+  
+ Q3) Disjoint Sets by rank and path compression methodology.
+  Operations:
+# To make set
+# To find the ultimate root of tree(FindPar)
+# To connect to sets if they are disjoint.
+
+//The program is very easy and is used in algorithms like Kruksal's MST.
+//Code:
+int parent[10001];
+int rank[10001];
+
+void makeSet(){
+     for(int i=0;i<=n;i++){
+          parent[i]=i;
+          rank[i]=0;
+     }
+}
+
+//Works in 4alpha time complexity, if path compression is not done, it worls in logarithmic!
+int findPar(int node){
+     if(node==parent[node]){
+          return node;
+     }
+     
+     //path compressing -> parent[node]==...
+     return parent[node]=findPar(parent[node]);
+}
+
+void union(int u, int v){
+     u=findPar(u);
+     v=findPar(v);
+     
+     if(rank[u]>rank[v]){
+          parent[v]=u;
+     }
+     
+     else if(rank[u]<rank[v]){
+          parent[u]=v;
+     }
+     
+     //rank or height of tree only changes or nicrease by 1
+     //when same height nodes are attached!
+     else{
+          parent[u]=v;
+          rank[v]++;
+     }
+}
+void hihi(){
+    makeSet();
+    
+    int m;
+    cin>>m;
+    
+    while(m--){
+         int u,v;
+         union(u,v);
+    }
+    
+    if(findPar(u)!=findPar(v)){
+         cout<<"Different components"<<endl;
+    }
+    
+    else cout<<"Same components!"<<endl;
+    return;
+}
+//if same components come -> means that graph contains cycle!
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+  Q4) Kruskal MST problem:
+/* 
+1) Sort all edges with respect to weight
+2) Greedily choose shortest edge
+3) If nodes don't belong to same component, take them
+4) Keep taking until the two nodes don't belong to the same set
+5) Don't pick if they are from same component.
+6) This way no cycle would be added and we get minimum cost
+7) Time complexity is O(m log m) + O(m * O(4alpha)) --> O(mlogm)
+8) Space complexity is O(N)
+*/
+
+//Creating sturcture
+struct node{
+  int u, v, wt;
+  node(int first,int second, int weight){
+       u=first;
+       v=second;
+       wt=weight;
+  }
+};
+
+//Works in 4alpha time complexity, if path compression is not done, it works in logarithmic!
+int findPar(int node, vector<int> &parent){
+     if(node==parent[node]){
+          return node;
+     }
+     
+     //path compressing -> parent[node]==...
+     return parent[node]=findPar(parent[node], parent);
+}
+
+//making cmp
+bool cmp(node one, node two){
+     return one.wt<two.wt;
+}
+
+void unionn(int u, int v, vector<int> &parent, vector<int> &rank){
+     u=findPar(u,parent);
+     v=findPar(v,parent);
+     
+     if(rank[u]>rank[v]){
+          parent[v]=u;
+     }
+     
+     else if(rank[u]<rank[v]){
+          parent[u]=v;
+     }
+     
+     //rank or height of tree only changes or nicrease by 1
+     //when same height nodes are attached!
+     else{
+          parent[u]=v;
+          rank[v]++;
+     }
+}
+
+void hihi(){
+   // makeSet();
+    int n,m;
+    cin>>n>>m;
+    
+    vector<node> edges;
+    
+    //Taking input in linear form!
+    for(int i=0;i<m;i++){
+         int u,v,wt;
+         cin>>u>>v>>wt;
+         
+         edges.pb(node(u,v,wt));
+    }
+    
+    //Sorting according to weight
+    sort(edges.begin(),edges.end(),cmp);
+    
+    vector<int> parent(n);
+    for(int i=0;i<n;i++){
+         parent[i]=i;
+    }
+    
+    vector<int> rank(n,0);
+    
+    int cost=0;
+    vector<pair<int,int>> mst;
+    
+    for(auto it: edges){
+         if(findPar(it.v , parent)!=findPar(it.u, parent)){
+              mst.push_back({it.u, it.v});
+              cost+=it.wt;
+              unionn(it.u, it.v, parent, rank);
+         }
+    }
+    
+    cout<<cost<<endl;
+    
+    for(auto it:mst){
+         cout<<it.first<<" "<<it.second<<endl;
+    }
+   
+    return;
+}
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+  
+  
