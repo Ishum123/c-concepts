f5c724ca70e3c0cabbdbfa8d2ffa1126108f790c
Yashasweni Mathur <94554260+Ishum123@users.noreply.github.com>
2022-07-23 14:46:18 +0530

    Update bst.cpp

--- bst.cpp
+++ bst.cpp
@@ -1,5 +1,9 @@
 # Facts: Successor and inorder successor are different things!
 
+//Inorder: root->left->right
+//Preorder: left->root->right
+//Postorder: left->right->root
+
 Q1) Deleting node in a BST
 # Three posibilities: whether a leaf node, a node with left/right child, root node(in this case inorder successor is needed)!
 
