7ecd2ceaf37d3cc4506f38d74c9326c3f806e4f1
Yashasweni Mathur <94554260+Ishum123@users.noreply.github.com>
2022-07-27 16:33:12 +0530

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -76,3 +76,64 @@ void hihi(){
     
     return;
 }
+
+
+Q2) KMP algo
+vector<int> kmp(string s){
+     int n=s.length();
+     vector<int> pre(n,0);
+     
+     for(int i=1;i<n;i++){
+          int j=pre[i-1];
+          
+          while(j>0 and s[i]!=s[j]){
+               //match nahi hore, to go to index jaha last match ho paa rhe ho!
+               j=pre[j-1];
+          }
+          
+          if(s[i]==s[j]){
+               j++; //match hogya to abhi tk jitna hua plus one
+          }
+          pre[i]=j;
+     }
+     return pre;
+}
+
+void hihi(){
+     string t="lolojaslol";
+     string s="as";
+     
+     vector<int> prefix=kmp(s);
+     
+     //for finding lps(longest proper prefix suffix) by kmp
+     /*for(int i=0;i<s.length();i++){
+          cout<<prefix[i]<<" ";
+     }
+     cout<<endl;*/
+     
+     //for searching s in t and returning index
+     //kmp s ka bnta he jise search krna hota uski table
+     int pos=-1;
+     int i(0),j(0);
+
+     while(i<t.size()){
+          if(t[i]==s[j]){
+               i++;
+               j++;
+          }
+          
+          else{
+               if(j!=0) j=prefix[j-1];//puraane index pr jaao
+               else i++;
+          }
+          
+          if(j==s.size()){//found a substring
+               pos=i-s.size();
+               break;
+          }
+     }
+     
+     cout<<pos<<endl;
+     return;
+}
+ 
